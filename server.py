46d3194be0a5dd7a48d373c7d4d2d19b4df53179
James Schulman <jaschul@bu.edu>
2017-09-09 18:35:57 -0400

    Updated tythe weather api to display temperature (K)

--- server.py
+++ server.py
@@ -1,17 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return "How are you?"
+    return render_template('input.html')
 
 
-@app.route('/weather/<arg>')
-def weather(arg):
-    return render_template('weather.html', arg=arg)
-
+@app.route('/weather', methods=['POST'])
+def weather():
+    zipcode = request.form['zip']
+    r = requests.get('http://api.openweathermap.org/data/2.5/weather?zip='+zipcode+',us&appid=902daaa31b338d0d36827a506b7353e3')
+    tempk = float(r.json()['main']['temp'])
+    return render_template('weather.html', temp=tempk)
 
 if __name__ == "__main__":
     app.run(debug=True)
