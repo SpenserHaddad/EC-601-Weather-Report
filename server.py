bb2aea6accb7d0d658cbb281d621a3df41790d5c
Spenser Haddad <spenser.haddad@gmail.com>
2017-09-12 23:02:03 -0400

    Added checks for location type and refactored
    No input validation on the location types yet (apart from making sure
    location isn't empty)

--- server.py
+++ server.py
@@ -1,19 +1,50 @@
-from flask import Flask, render_template, request
+import re
+import ipaddress
+from flask import Flask, render_template, request, redirect, url_for
 import weather
 
 app = Flask(__name__)
-#12312
+
 
 @app.route('/')
-def hello():
+def index():
     return render_template('input.html')
 
 
-@app.route('/weather', methods=['POST'])
+@app.route('/weather', methods=['POST', 'GET'])
 def weather_report():
-    zipcode = request.form['zip']
-    tempk = weather.get_weather_by_zip(zipcode).temperatures
-    return render_template('weather.html', temp=tempk)
+    if request.method == 'POST':
+        return redirect(url_for('weather_report',
+                                location=request.form['location']))
+    else:
+        location = request.args['location'].strip()
+
+        # Determine the type of location used
+        if not location:
+            return render_template('weather.html', failed=True)
+        elif _location_is_zip(location):
+            report = weather.get_weather_by_zip(location)
+        elif _location_is_ip_addr(location):
+            report = weather.get_weather_by_ip(location)
+        else:  # Assume it's a city
+            city, country = location.split(',')
+            report = weather.get_weather_by_city(city.strip(), country.strip())
+        return render_template('weather.html',
+                               temp=report.temperatures, failed=False)
+
+
+def _location_is_zip(location):
+    # Match pattern of 5 numbers (assume we would only get valid zip codes)
+    return bool(re.match(r'\d{5}', location))
+
+
+def _location_is_ip_addr(location):
+    try:
+        # If this runs without problems then it's a valid ip
+        ipaddress.ip_address(location)
+        return True
+    except ValueError:
+        return False
 
 
 if __name__ == "__main__":
--- weather.py
+++ weather.py
@@ -7,55 +7,55 @@ Functions:
     get_weather_by_coordinate(long, lat)
 """
 from collections import namedtuple
-#import os
 import requests
 
-#WEATHER_API_URL = r'http://api.openweathermap.org/data/2.5/weather'
 WEATHER_API_URL = r'http://api.openweathermap.org/data/2.5/forecast'
 API_KEY = '902daaa31b338d0d36827a506b7353e3'
 GEO_IP_URL = r'http://freegeoip.net/json'
 
+Weather = namedtuple('Weather', ('temperatures', 'weather'))
 
-Weather = namedtuple('Weather', ('temperatures'))
 
 def get_weather_by_zip(zip_code, country_code='us'):
     params = {'appid': API_KEY,
               'zip': f'{zip_code},{country_code}'}
     r = requests.get(WEATHER_API_URL, params=params)
     weather_data = r.json()
-    temp=[]
-    for i in range(5) :
-        temp.append(weather_data['list'][i]['main']['temp'])
-    return Weather(temperatures=temp)
-
-def get_weather_by_zip_1(zip_code, country_code='us'):
-    params = {'appid': API_KEY,
-              'zip': f'{zip_code},{country_code}'}
-    r = requests.get(WEATHER_API_URL, params=params)
-    weather_data = r.json()
-    temp=[]
-    temp.append(weather_data['list'][0]['main']['temp'])
-    return Weather(temperatures=temp)
+    return unpack_weather_data(weather_data)
 
 
 def get_weather_by_ip(ip):
-    lat, long_ = _get_ip_coordinates(ip)
-    return get_weather_by_coordinate(lat, long_)
+    lat, lon = _get_ip_coordinates(ip)
+    return get_weather_by_coordinate(lat, lon)
 
 
-def get_weather_by_coordinate(lat, long_):
-    params = {'lat': lat, 'long': long_}
+def get_weather_by_coordinate(lat, lon):
+    params = {'lat': lat, 'lon': lon, 'appid': API_KEY}
     r = requests.get(WEATHER_API_URL, params=params)
     weather_data = r.json()
-    temp=[]
-    for i in range(5) :
-        temp.append(weather_data['list'][i]['main']['temp'])
-    return Weather(temperatures=temp)
-#    return Weather(temparature=weather_data['main']['temp'])
+    return unpack_weather_data(weather_data)
+
+
+def get_weather_by_city(city, country):
+    pass
+
+
+def unpack_weather_data(weather_data):
+    data = weather_data['list']
+
+    temp = []
+    temp.append(data[0]['main']['temp'])
+    temp.append(data[1]['main']['temp'])
+    temp.append(data[2]['main']['temp'])
+    temp.append(data[3]['main']['temp'])
+    temp.append(data[4]['main']['temp'])
+
+    weathers = [data['weather'][0]['main'] for data in data[0:5]]
+    return Weather(temperatures=temp, weather=weathers)
 
 
 def _get_ip_coordinates(ip):
     params = {'q': ip}
-    r = requests.gt(GEO_IP_URL, params=params)
+    r = requests.get(GEO_IP_URL, params=params)
     geo_data = r.json()
     return (geo_data['latitude'], geo_data['longitude'])
