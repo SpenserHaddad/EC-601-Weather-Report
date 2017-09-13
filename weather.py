b04e364ef1cf43102fa8dbb728112cbd5f1bf650
jweixuan <jwx0728@bu.edu>
2017-09-12 20:00:51 -0400

    Null

--- weather.py
+++ weather.py
@@ -15,9 +15,6 @@ WEATHER_API_URL = r'http://api.openweathermap.org/data/2.5/forecast'
 API_KEY = '902daaa31b338d0d36827a506b7353e3'
 GEO_IP_URL = r'http://freegeoip.net/json'
 
-#Weather = namedtuple('Weather', ('temperature', 'weather'))
-#temperatures = namedtuple('temperature_1','temperature_2',
-#                          'temperature_3','temperature_4','temperature_5')
 
 Weather = namedtuple('Weather', ('temperatures'))
 
@@ -27,12 +24,8 @@ def get_weather_by_zip(zip_code, country_code='us'):
     r = requests.get(WEATHER_API_URL, params=params)
     weather_data = r.json()
     temp=[]
-    temp.append(weather_data['list'][0]['main']['temp'])
-    temp.append(weather_data['list'][1]['main']['temp'])
-    temp.append(weather_data['list'][2]['main']['temp'])
-    temp.append(weather_data['list'][3]['main']['temp'])
-    temp.append(weather_data['list'][4]['main']['temp'])
-    print(temp)
+    for i in range(5) :
+        temp.append(weather_data['list'][i]['main']['temp'])
     return Weather(temperatures=temp)
 
 def get_weather_by_zip_1(zip_code, country_code='us'):
@@ -43,24 +36,26 @@ def get_weather_by_zip_1(zip_code, country_code='us'):
     temp=[]
     temp.append(weather_data['list'][0]['main']['temp'])
     return Weather(temperatures=temp)
-#    return Weather(temperature=temp,
-#                   weather=weather_data['weather'][0]['main'])#weather是天气状况：多云....
-#
-#def get_weather_by_ip(ip):
-#    lat, long_ = _get_ip_coordinates(ip)
-#    return get_weather_by_coordinate(lat, long_)
 
 
-#def get_weather_by_coordinate(lat, long_):
-#    params = {'lat': lat, 'long': long_}
-#    r = requests.get(WEATHER_API_URL, params=params)
-#    weather_data = r.json()
-#    return Weather(temparature=weather_data['main']['temp'],
-#                   weather=weather_data['weather'][0]['main'])
+def get_weather_by_ip(ip):
+    lat, long_ = _get_ip_coordinates(ip)
+    return get_weather_by_coordinate(lat, long_)
 
 
-#def _get_ip_coordinates(ip):
-#    params = {'q': ip}
- #   r = requests.gt(GEO_IP_URL, params=params)
-#    geo_data = r.json()
-#    return (geo_data['latitude'], geo_data['longitude'])
+def get_weather_by_coordinate(lat, long_):
+    params = {'lat': lat, 'long': long_}
+    r = requests.get(WEATHER_API_URL, params=params)
+    weather_data = r.json()
+    temp=[]
+    for i in range(5) :
+        temp.append(weather_data['list'][i]['main']['temp'])
+    return Weather(temperatures=temp)
+#    return Weather(temparature=weather_data['main']['temp'])
+
+
+def _get_ip_coordinates(ip):
+    params = {'q': ip}
+    r = requests.gt(GEO_IP_URL, params=params)
+    geo_data = r.json()
+    return (geo_data['latitude'], geo_data['longitude'])
